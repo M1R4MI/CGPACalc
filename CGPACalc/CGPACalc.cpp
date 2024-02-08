7db41fe97bf1eeca9b2ec415e3be141d693e1d61
Vadym Mikheiev <85171861+M1R4MI@users.noreply.github.com>
2024-02-08 13:35:45 +0200

    Main version for simple CGPR calculator
    Simple study project.

---
+++ CGPACalc/CGPACalc.cpp
@@ -0,0 +1,35 @@
+﻿#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+static double CGPA(int grade[], const int subjects)
+{
+	double result = 0;
+	for(auto i = 0; i < subjects; i++)
+	{
+		result += grade[i];
+	}
+	result = result / subjects;
+	return result;
+}
+
+int main()
+{
+    int subjects_сount = 0;
+	
+	cout << "Write count of major subjects: ";
+	cin >> subjects_сount;
+
+	auto* grades = new int[subjects_сount];
+
+	for(auto i = 0; i < subjects_сount; i++)
+	{
+		cout << "Write grade for subject " << i + 1 <<": ";
+		cin >> grades[i];
+	}
+
+	const auto res = CGPA(grades, subjects_сount);
+
+	cout << "Result is: " << res << endl;
+}
